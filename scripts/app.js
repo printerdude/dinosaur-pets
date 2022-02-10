e2a53c00c78352e95fe19e03b3429164cef4d780
Brian Yoon <infobyoon@gmail.com>
2022-02-09 17:21:19 -0800

    Commit 12: added clearinterval method to functions that increase pet stats

--- scripts/app.js
+++ scripts/app.js
@@ -5,22 +5,25 @@ let sleepiness = 1;
 
 
 
-const currentImage = $(".currentImage");
-const currentTitle = $(".currentTitle");
+
 // Increase petAge
 // setInterval(updateTimer, 1000);
+// Change Pet Images to reflect evolution
+const currentImage = $(".currentImage");
+const currentTitle = $(".currentTitle");
+
 const increaseAge = () => {
     age++;
     $("#petAge").text('Age: ' + age);
-
     // Changing Current Image
     if (age === 10) {
-        currentTitle.text('Stage 2: Charmeleon');
+        currentTitle.text('2nd Evolution');
         currentImage.attr("src", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fusercontent2.hubstatic.com%2F13811955_f1024.jpg&f=1&nofb=1");
     }
     if (age === 20) {
-        currentTitle.text('Stage 3: Charizard');
+        currentTitle.text('Final Evolution');
         currentImage.attr("src", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fvignette2.wikia.nocookie.net%2Fsonicpokemon%2Fimages%2Fb%2Fbf%2FCharizard_AG_anime.png%2Frevision%2Flatest%3Fcb%3D20130714192025&f=1&nofb=1");
+        clearInterval(ageTimer);
     }
 }
 
@@ -30,31 +33,43 @@ const increaseAge = () => {
 
 // Increase petStats to reflect growing hunger,boredom,and sleepiness.
 const increaseHunger = () => {
-    // console.log("this function will increase stats incrementally every 10 seconds until the hunger is 10");
     hunger++;
     $("#hungerStat").text('Hunger: ' + hunger);
     if (hunger === 10) {
         alert("Your dino has ran away. You will never see them again. Refresh the page to restart the game.");
+        clearInterval(ageTimer);
+        clearInterval(hungerTimer);
+        clearInterval(boredomTimer);
+        clearInterval(sleepinessTimer);
     }
 }
-const increaseBoredom = () => {
-    // console.log("this function will increase stats incrementally every 10 seconds until the boredom is 10");
+const increaseBoredom = () => {;
     boredom++;
     $("#boredomStat").text('Boredom: ' + boredom);
     if (boredom === 10) {
         alert("Your dino has ran away. You will never see them again. Refresh the page to restart the game.");
+        clearInterval(ageTimer);
+        clearInterval(hungerTimer);
+        clearInterval(boredomTimer);
+        clearInterval(sleepinessTimer);
     }
 }
 const increaseSleepiness = () => {
-    // console.log("this function will increase stats incrementally every 10 seconds until the sleepiness is 10");
     sleepiness++;
     $("#sleepinessStat").text('Sleepiness: ' + sleepiness);
     if (sleepiness === 10) {
         alert("Your dino has ran away. You will never see them again. Refresh the page to restart the game.");
+        clearInterval(ageTimer);
+        clearInterval(hungerTimer);
+        clearInterval(boredomTimer);
+        clearInterval(sleepinessTimer);
     }
 }
 
-
+let ageTimer;
+let hungerTimer;
+let boredomTimer;
+let sleepinessTimer;
 
 
 // Start Button
@@ -64,14 +79,14 @@ startButton.on("click", () => {
 	increaseHunger();
     increaseBoredom();
     increaseSleepiness();
-    setInterval(increaseAge, 500);
-	setInterval(increaseHunger, 1000);
-    setInterval(increaseBoredom, 1000);
-    setInterval(increaseSleepiness, 1000);
+    ageTimer = setInterval(increaseAge, 500);
+	hungerTimer = setInterval(increaseHunger, 1000);
+    boredomTimer = setInterval(increaseBoredom, 1000);
+    sleepinessTimer = setInterval(increaseSleepiness, 1000);
 });
 
 
-// Feed, play, lights off buttons
+// Feed, play, lights off buttons for User Action
 const feed = $(".feed");
 feed.on("click", (event) =>{
     hunger--;
@@ -88,4 +103,9 @@ lightsOff.on("click", (event) =>{
 });
 
 
-
+const stopGame = function () {
+clearInterval(increaseAge());
+// clearInterval(sleepInterval);
+// clearInterval(sleepInterval);
+// clearInterval(sleepInterval);
+}
